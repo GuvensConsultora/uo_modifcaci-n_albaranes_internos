858521f05ddfca0bcb3b38b64f53cdfd116ba88b
GuvensConsultora <guvensconsultora@gmail.com>
2025-09-26 12:19:25 -0300

    Adaptar albaranes

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tutorial: Heredar reporte de albarán",
+    "version": "17.0.1.0.0",
+    "summary": "Primeros pasos heredando QWeb del albarán",
+    "author": "Tú",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "report/deliveryslip_inherit.xml",
+    ],
+    "installable": True,
+}
